240b598732a778f8d5fa25bd7895e52eb32b22bd
Coffroth <morgan.coffroth@wsu.edu>
2022-12-12 23:18:34 +0000

    updated typos, pictures, addition: reset button

--- scripts.js
+++ scripts.js
@@ -50,9 +50,11 @@ function Scenario(ScenarioName, routeURL, imageURL) {
 
   this.displayScenario = function() {
     
-    document.querySelector("#HeadText h1").innerHTML = this.ScenarioName;
+    document.querySelector(".HeadText h1").innerHTML = this.ScenarioName;
+    
+//       document.querySelector("#HeadTextNoImg h1").innerHTML = this.ScenarioName;
 
-      document.querySelector("#HeadText").style.backgroundImage = "url(" + this.imageURL + ")";
+      document.querySelector(".HeadText").style.backgroundImage = "url(" + this.imageURL + ")";
 //     document.getElementById('startButton').onclick = startNo;
     loadFileInto(this.route, "#Question");
   }
@@ -80,8 +82,69 @@ okScenario = new Scenario("College Bound Day 14", "B.html", "https://images.unsp
 avgScenario = new Scenario("College Bound Day 14", "C.html", "https://images.unsplash.com/photo-1612215670548-612dd2de09ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1746&q=80.jpg")
 
 sadScenario = new Scenario("College Bound Day 14", "F.html", "https://images.unsplash.com/photo-1521075486433-bf4052bb37bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1344&q=80.jpg")
+  
+soScenario = new Scenario("College Bound", "trackScore.html", "https://images.unsplash.com/photo-1527720255604-b27935ade401?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80.jpg")
 }
 
+// function TryAgain( ResetName, ResetURL, imageReset) {
+
+//   this.ResetName = ResetName;
+//   this.ResetURL = ResetURL;
+//   this.imageReset = imageReset;
+  
+
+//   this.displayTryAgain = function() {
+    
+//     document.querySelector("#HeadText h2").innerHTML = this.ResetName;
+
+//       document.querySelector("#HeadTextNoImg").style.backgroundImage = "url(" + this.imageReset + ")";
+// //     document.getElementById('startButton').onclick = startNo;
+//     loadFileInto(this.ResetURL, "#Question");
+//   }
+// }
+
+// {
+// soTryAgain = new TryAgain("College Bound", "trackScore.html", "https://images.unsplash.com/photo-1527720255604-b27935ade401?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80.jpg")
+// }
+
+
+//RESET BUTTON TRYS//
+
+// function resetScenario(resetScenarioName, resetRouteURL) {
+
+//   this.resetScenarioName = resetScenarioName;
+//   this.reetRoute = resetRouteURL;
+  
+
+//   this.displayresetScenario = function() {
+    
+//     document.querySelector("#HeadText h1").innerHTML = this.resetScenarioName;
+
+// //     document.getElementById('startButton').onclick = startNo;
+//     loadFileInto(this.resetRoute, "#Question");
+//   }
+// }
+
+// startresetScenario = new resetScenario("College Bound", "index.html")
+
+
+// function Reset(resetName, routeURL, imageURL) {
+
+//   this.resetName = resetName;
+//   this.route = routeURL;
+//    this.imageURL = imageURL;
+
+//   this.displayReset = function() {
+    
+//    document.querySelector("#HeadText h1").innerHTML = this.resetName;
+// document.querySelector("#HeadText").style.backgroundImage = "url(" + this.imageURL + ")";
+//     loadFileInto(this.route, "#Question");
+//   }
+// }
+
+//   startOverReset = new Reset( "College Bound", "index.html", "https://images.unsplash.com/photo-1527720255604-b27935ade401?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80.jpg")
+
+//PHOTO CRED FUNCTION TRY//
 
 // function PhotoCred ( photoName, imgSource){
 //   this.photoName = photoName
@@ -94,6 +157,8 @@ sadScenario = new Scenario("College Bound Day 14", "F.html", "https://images.uns
 // }
 
 //  BeginGamePhotoCred = new PhotoCred("unsplash", "StartGame.html")
+
+//SCORE TRACKER TRY//
 
 // var scoreTot = 5;
 
@@ -115,15 +180,3 @@ sadScenario = new Scenario("College Bound Day 14", "F.html", "https://images.uns
 // }
 
 
-// window.onload = function() {
-
-//   document.querySelector("#startButton").onclick = function() {
-//     BeginGameScenario.displayScenario();
-//   }
-//   document.querySelector("#firstButton").onclick = function() {
-//     OneWeekScenario.displayScenario();
-//   }
-//   document.querySelector("#secondButton").onclick = function() {
-//     TwoWeeksScenario.displayScenario();
-//   }
-// }
